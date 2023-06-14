e5e8f2c5fff2689ad8d9dec9efe12cf615abc540
Gordon Cheung <gordonhwc1@gmail.com>
2023-06-13 23:31:09 -0700

    add new column to users

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Matches } from "class-validator";
 import { Review } from "./Review";
 import { emailRe, pwdRe } from "../constants/regex";
-
+import { Role } from "../types/roles";
 @Entity("users")
 export class User {
   @PrimaryGeneratedColumn()
@@ -15,6 +15,9 @@ export class User {
   @Column()
   @Matches(pwdRe)
   password: String;
+
+  @Column()
+  role: Role;
 
   @OneToMany(() => Review, (r) => r.userID)
   reviews: Array<Review>;
