b4cc900feea86ea25a9d6cc2fbf9458a084b09a3
ryanmckim <mck649@gmail.com>
2023-07-25 21:58:48 -0700

    Updated file names

---
+++ backend/src/models/User.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Matches } from "class-validator";
+import { Review } from "./Review";
+import { emailRe, pwdRe } from "../constants/regex";
+import { Role } from "../types/roles";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  @Matches(emailRe)
+  email: String;
+
+  @Column()
+  @Matches(pwdRe)
+  password: String;
+
+  // @OneToMany(() => Review, (review) => review.userID)
+  @Column({ type: "jsonb", default: [] })
+  reviews: Review[];
+
+  @Column()
+  role: Role;
+}
