49562ec05ee7ce73ba9b461a77b87b678e4b4c1d
ryanmckim <mck649@gmail.com>
2023-08-10 01:16:14 -0700

    Implemented buttons, will need to fix positioning

--- frontend/src/components/Company/Company.tsx
+++ frontend/src/components/Company/Company.tsx
@@ -1,8 +1,14 @@
-import { Card, CardHeader, CardBody, Heading, Center, SimpleGrid, Text, Box} from '@chakra-ui/react'
+import { Card, Heading, Center, SimpleGrid, Text, Box, Select, HStack} from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
+import './company.scss'
 
 export default function Company() {
-    const [company, setCompany] = useState([])
+    const [company, setCompany] = useState([]);
+    const [currency, setCurrency] = useState("CAD");
+
+    const changeCurrency = () => {
+      setCurrency(currency === "CAD" ? "USD" : "CAD");
+    }
 
     useEffect(() => {
       const fetchCompanies = async () => {
@@ -20,11 +26,13 @@ export default function Company() {
     return (
       <React.Fragment>
         <Center>
-          {/* <Card boxShadow="0 3px 10px rgb(0 0 0 / 0.2)"
+          <Card boxShadow="0 3px 10px rgb(0 0 0 / 0.2)"
                 bg="white"
                 borderRadius="5px"
-                maxWidth={600}> */}
-          <Card>
+                maxWidth={600}
+                padding="3rem"
+                margin="1rem">
+          {/* <Card> */}
             <SimpleGrid columns={3} spacing={4}>
               <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
                 <Heading fontSize="60px" fontFamily="arial">SAP</Heading>
@@ -38,6 +46,26 @@ export default function Company() {
             </SimpleGrid>
           </Card>
         </Center>
+        <HStack>
+          <Box w='200px'>
+            <Select placeholder='Sort by'>
+              <option>Most Recent</option>
+              <option>Least Recent</option>
+              <option>Highest salary</option>
+              <option>Lowest salary</option>
+            </Select>
+          </Box>
+          <Box textAlign="center" p={10}>
+            <div className='toggle-button-group' onClick={changeCurrency}>
+              <div className='toggle-button' style={{backgroundColor: currency === "CAD" ? '#3498db' : '#fff'}}>
+                <h1 className='toggle-button-text' style={{color: currency === "CAD" ? '#fff' : '#3498db' }}>CAD</h1>
+              </div>
+              <div className='toggle-button' style={{backgroundColor: currency === "USD" ? '#3498db' : '#fff'}}>
+                <h1 className='toggle-button-text' style={{color: currency === "USD" ? '#fff' : '#3498db' }}>USD</h1>
+              </div>
+            </div>
+          </Box>
+        </HStack>
       </React.Fragment>
     )
 }
