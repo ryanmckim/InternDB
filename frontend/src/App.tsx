6bfe5a02e620390d1f7f0188bb7ad3b1908d778e
Ryan Kim <72713726+ryanmckim@users.noreply.github.com>
2023-08-20 23:09:55 -0700

    Company Component (#13)
    * create react app ts
    
    * file structure
    
    * SASS & Chakra UI
    
    * Initial setup
    
    * Naming issue commit
    
    * Fixed file name issue
    
    * Company Header
    
    * Implemented buttons, will need to fix positioning
    
    * Move buttons
    
    * removed placeholder in select
    
    * add sass
    
    ---------
    
    Co-authored-by: Gordon Cheung <gordonhwc1@gmail.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Company from "./components/Company/Company"
+
+function App() {
+  return (
+    <div>
+      <Company/>
+    </div>
+  );
+}
+
+export default App;
