05631cd2a9da9f5ee81f1b54fb0531ccceb4d4db
ryanmckim <mck649@gmail.com>
2023-08-22 10:52:24 -0700

    Finished routing for components

---
+++ backend/src/controllers/company.ts
@@ -0,0 +1,3 @@
+export const displayReview = () => {};
+
+export const displayCompanies = () => {};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import Company from "./components/Company/Company"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Company from "./components/Company/Company";
+import Profile from './components/Profile/Profile';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
-    <div>
-      <Company/>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
