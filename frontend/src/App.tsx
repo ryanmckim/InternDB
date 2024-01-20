b716b78177b8817829f4ddb63acb27509e81ec31
Gordon Cheung <georgehwc@gmail.com>
2024-01-20 11:56:24 -0800

    add login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,27 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Company from "./components/Company/Company";
-import Profile from './components/Profile/Profile';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import Profile from "./components/Profile/Profile";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Verification from "./components/Verification/Verification";
+import VerificationNotification from "./components/Verification/VerificationNotification";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/register" element={<Register/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-          <Route path="/company/:id" element={<Company/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/company/:id" element={<Company />}></Route>
+          <Route path="/verify/:token" element={<Verification />}></Route>
+          <Route
+            path="/email-sent"
+            element={<VerificationNotification />}
+          ></Route>
         </Routes>
       </div>
     </Router>
